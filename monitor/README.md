01ee6563869726bdd6aca253e8617c5b8abbacea
5201314999 <1446418561@qq.com>
2019-10-30 16:58:23 +0800

    增加了一个小例子，关于前端监控问题

---
+++ monitor/README.md
@@ -0,0 +1,10 @@
+# 前端上报测试
+
+## start the project
+need npm and nodejs
+1. npm install
+
+2. npm start
+
+open url:http://127.0.0.1:3000
+and  http://127.0.0.1:8089 ,and then refresh http://127.0.0.1:3000 
---
+++ monitor/server/app.js
@@ -0,0 +1,32 @@
+const Koa=require('koa')
+const Router=require('koa-router')
+const koaStatic=require('koa-static')
+const path=require('path')
+
+const app=new Koa()
+
+const router=new Router()
+
+const errorInfo=[]
+
+router.get('/',async ctx=>{
+    ctx.body=`<div>欢迎您来到监控后台首页</div><div style="color:red">报错信息：<br>${errorInfo.join(" ")}<br></div>`
+}).get('/alarm',async ctx=>{
+    errorInfo.push(ctx.query.error)
+    ctx.response.status=200
+})
+
+
+app.use(router.routes()).use(router.allowedMethods)
+
+app.listen('3000',async ()=>{
+    console.log('server is starting at port 3000,http://127.0.0.1:3000');
+})
+
+const webApp=new Koa()
+webApp.use(koaStatic(path.join(__dirname,'../html/')))
+console.log(__dirname)
+webApp.listen('8089',async ()=>{
+    console.log('webapp is starting at port 8089,http://127.0.0.1:8089');
+})
+
